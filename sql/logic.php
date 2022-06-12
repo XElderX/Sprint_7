6fa80cd077fb74b8b11934559e794209f55756c0
XElderX <dalius.badley@gmail.com>
2022-06-12 21:04:16 +0300

    functionalities implementation stage: Added delete button; functionility to delete persons and projects from db

--- sql/logic.php
+++ sql/logic.php
@@ -6,6 +6,27 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+//delete personell logic
+if(isset($_GET['action']) and $_GET['action'] == 'delete'){
+    $sql = 'DELETE FROM personell WHERE id = ?';
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param('i', $_GET['id']);
+    $res = $stmt->execute();  
+    $stmt->close();
+    header("Location: " . strtok($_SERVER["REQUEST_URI"], '?'));
+    die();
+}
+//delete project logic
+if(isset($_GET['action']) and $_GET['action'] == 'project_delete'){
+    $sql = 'DELETE FROM projects WHERE Project_id= ?';
+    $stmt = $connection->prepare($sql);
+    var_dump($stmt);
+    $stmt->bind_param('i', $_GET['Project_id']);
+    $res = $stmt->execute();
+    $stmt->close();
+    header("Location: " . strtok($_SERVER["REQUEST_URI"], '?'));
+    die();
+}
 
 //function for personell data render
 function personell($connection){
@@ -19,14 +40,14 @@ echo "<br><tr><th>id</th><th>name</th><th>lastname</th><th>Projects</th><th>Acti
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo ("<tr><td>" . $row['id'] . "</td><td> ". $row['First_name'] . "</td> <td>" . $row['Last_name'] . "</td><td>" . $row['Project_name'] . "</td><td>". "</td></tr> <br>");
+        echo ("<tr><td>" . $row['id'] . "</td><td> ". $row['First_name'] . "</td> <td>" . $row['Last_name'] . "</td><td>" . $row['Project_name'] . "</td><td>" ."<button>" . '<a href="?action=delete&id=' . $row['id'] . '">Delete</a></button>' . "</td></tr> <br>");
     }
 } else {
     echo "0 results";
 }
 
 }
-//function To render project lists 
+//function ro render project lists 
 function project($connection){
     $sql = "SELECT 
     projects.Project_id, projects.Project_name, group_concat(Concat_ws(' ', personell.First_name, Personell.Last_Name) separator ',') AS assigned 
@@ -40,7 +61,7 @@ echo "<br><tr><th>Project</th><th>Assigned</th><th>Actions</th></tr>";
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo ("<tr><td>" . $row['Project_name'] .  "</td><td>". $row['assigned'] . "</td><td>" ."</td></tr> <br>");
+        echo ("<tr><td>" . $row['Project_name'] .  "</td><td>". $row['assigned'] . "</td><td>" ."<button>" . '<a href="?action=project_delete&Project_id=' . $row['Project_id'] . '">Delete</a></button>' . "</td></tr> <br>");
     }
 } else {
     echo "0 results";
